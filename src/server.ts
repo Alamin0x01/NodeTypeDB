2ae0ba8991dce680e24e44338f1eaab5d20199af
Md. Alamin A <md.alamin.ali.096@gmail.com>
2023-11-24 00:14:04 +0600

    typescrip or expressJS applied and MongoDB connected

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import config from "./app/config";
+import app from "./app";
+
+async function main() {
+  try {
+    await mongoose.connect(config.DB_URL as string);
+    app.listen(config.PORT, () => {
+      console.log(`App listening on port ${config.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
