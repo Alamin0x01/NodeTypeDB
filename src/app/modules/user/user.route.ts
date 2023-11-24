43b62eccad14410ea6753905e55e8905944d82dc
Md. Alamin A <md.alamin.ali.096@gmail.com>
2023-11-24 15:03:14 +0600

    GET-list of all users

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,5 +4,7 @@ import { UserController } from './user.controller';
 const router: Router = express.Router();
 
 router.post('/', UserController.createUser);
+// Endpoint: GET /api/users to Retrieve a list of all users
+router.get('/', UserController.getAllUsers);
 
 export const userRoutes = router;
--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -6,7 +6,16 @@ const createUserToDb = async (user: UserInterfaceIUser) => {
   const result = await UserModel.create(user);
   return result;
 };
+//getting all users from db
+const getAllUsersFromDb = async () => {
+  const result = await UserModel.find(
+    {},
+    { username: 1, fullName: 1, age: 1, email: 1, address: 1, _id: 0 },
+  );
+  return result;
+};
 
 export const userServices = {
   createUserToDb,
+  getAllUsersFromDb,
 };
